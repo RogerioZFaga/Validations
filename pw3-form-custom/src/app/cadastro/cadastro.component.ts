7e2a459632ea77daadae7799b2ae6f4229c8f274
RogerioZFaga <rogeriozfaga@gmail.com>
2025-05-27 10:59:11 -0300

    formcustom

---
+++ pw3-form-custom/src/app/cadastro/cadastro.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormGroup, ReactiveFormsModule, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-cadastro',
+  imports: [ReactiveFormsModule],
+  templateUrl: './cadastro.component.html',
+  styleUrl: './cadastro.component.css'
+})
+export class CadastroComponent {
+
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      username: ['', Validators.required],
+      passwords: this.fb.group({
+        password: ['', Validators.required],
+        confirmPassword: ['', Validators.required]
+      },
+      {validators: this.mesmaSenhaValidator})
+    });
+  }
+
+  submit() {
+    if (this.form.valid) {
+      alert('Formulário válido');
+    } else {
+      alert('Formulário inválido');
+    }
+  }
+
+  mesmaSenhaValidator: ValidatorFn = (formGroup: AbstractControl): ValidationErrors | null => {
+    let senha = formGroup.get('password')?.value;
+    let confirmarSenha = formGroup.get('confirmPassword')?.value;
+
+    if (senha !== confirmarSenha) {
+      return {senhasDiferentes: true};
+    }
+
+    return null;
+  }
+
+
+
+
+}
